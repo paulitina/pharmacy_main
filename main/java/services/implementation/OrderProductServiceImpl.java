c53ab78ac05e18e9fad070fa0f4ebabc8bc6f377
paulitina <langeely@mail.ru>
2021-06-26 23:34:57 +0300

    Deleted extra directories

--- main/java/services/implementation/OrderProductServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package services.implementation;
-
-import org.springframework.stereotype.Service;
-import services.api.OrderProductService;
-
-@Service
-public class OrderProductServiceImpl implements OrderProductService {
-}
